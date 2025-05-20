75fd6569f68db2b3ec6b2d20c50bf47d6034fa3d
SaJaL1164 <hindiawping@gmail.com>
2025-05-20 17:07:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const corsAnywhere = require("cors-anywhere");
+
+const port = process.env.PORT || 8080;
+
+corsAnywhere.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ["origin", "x-requested-with"],
+  removeHeaders: ["cookie", "cookie2"],
+}).listen(port, () => {
+  console.log(`CORS proxy running on port ${port}`);
+});
